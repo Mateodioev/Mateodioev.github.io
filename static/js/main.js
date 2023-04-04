5f986921df586c1b98470cadc1288f6b6ec72058
Mateo O <orehurcomito@gmail.com>
2023-04-04 18:42:29 -0500

    add hiden/view functions

--- static/js/main.js
+++ static/js/main.js
@@ -6,3 +6,39 @@ function closeNav(){
 	document.getElementById("mySidenav").style.width="0";
 
 }
+
+function viewAboutme() {
+	document.getElementById('aboutme').style.display = "inline-block";
+	hideExp();
+	hideProyects();
+	hideContactme();
+}
+
+function hideAboutme() { document.getElementById('aboutme').style.display = "none";}
+
+function viewExp() {
+	document.getElementById('exp').style.display = "inline-block";
+	hideAboutme();
+	hideProyects();
+	hideContactme();
+}
+
+function hideExp() { document.getElementById('exp').style.display = "none"; }
+
+function viewProyects() {
+	document.getElementById('proyects').style.display = "inline-block";
+	hideAboutme();
+	hideExp();
+	hideContactme();
+}
+
+function hideProyects() { document.getElementById('proyects').style.display = "none"; }
+
+function viewContactme() {
+	document.getElementById('contactme').style.display = "inline-block";
+	hideAboutme();
+	hideExp();
+	hideProyects();
+}
+
+function hideContactme() { document.getElementById('contactme').style.display = "none"; }
